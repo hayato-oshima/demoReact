73d9d2f282477d014e42a67a501893bb6ece348b
hayato-oshima <23fi015@ms.dendai.ac.jp>
2025-10-07 18:16:27 +0900

    1007

---
+++ src/components/Square.tsx
@@ -0,0 +1,5 @@
+export default function Square({value, onSquareClick} : {value: string | null, onSquareClick: () => void}){
+    return <button className="square" onClick={onSquareClick}>
+                {value}
+            </button>
+}
